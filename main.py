755e53a3d806f57302ea4b16eff8f2c8f9ac12f8
Kapix41 <125579556+Kapix41@users.noreply.github.com>
2023-02-16 16:20:11 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+
+#drukowanie na  ekranie
+#print ("kupa")
+
+# pytanie usera o cos
+#input()
+
+#dlugosc slowa x
+#print(len(x))
+
+#pisze druga litere wyrazu
+#print(zmienna[2])
+
+x = "Kacper"
+y = "Pawęska"
+z = x + " " + y #wypisuje dwie zamienne na raz ze spacja
+
+#wypisuje zmienne
+
+print(z)
+
+print(f"dkahgfioy {x} seadslh dfglksad skj lsd") #prostszy sposob formatowania tekstu
+
+a = input("Podaj swoj wiek")
+print (int(a) + 2) # int okresla ze 2 to liczba a nie zmienna
+
+james_bond = 7
+print(str(james_bond).zfill(3))
